fc55b7c8bcddab29c3c93afd117c085633d12a37
Jose Rankis <141494710+joserkis@users.noreply.github.com>
2023-08-13 01:07:49 +0300

    Update hooks.py

--- ext/hooks.py
+++ ext/hooks.py
@@ -4,28 +4,13 @@
 """
 
 class TemplateProcessor(object):
-    """ Derive from TemplateProcessor to provide an alternative template processor
-        during transformations. See :class:`~stubo.ext.transformer.TransformerBase`
-    """
 
     def eval_text(templ, request, **kwargs):
-        """ Evaluate a template and return the result. 
-        
-         :Params:
-          - `request`: the current request. See :class:`~stubo.model.request.Request`
-          - `**kwargs`: additional keyword arguments including
-                 stub - see :class:`~stubo.model.stub.Stub`
-        """
+
         pass
     
 
 class Hooks(object):
-    """ Derive from Hooks class to provide alternative transformer.
-    
-        The Hook is defined via the stubo config file. The default implementation is
-        
-        hooks_cls = stubo.ext.transformer.StuboDefaultHooks
-    """
     
     def make_transformer(self, stub):
         """ Factory method to create an instance of :class:`~stubo.ext.transformer.TransformerBase`
